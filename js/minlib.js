170d97a3db171d6bdf4cfa2b7e15159c32977d07
longzhou <pancnlz@gmail.com>
2013-06-09 15:02:03 +0800

    ajax bit & on bit

--- js/minlib.js
+++ js/minlib.js
@@ -4,7 +4,7 @@
 
         rQuickExpr = /^(?:#([\w-]+)|(\w+)|\.([\w-]+))$/,
 
-        vlib = function(selector) {
+        minlib = function(selector) {
             return new init(selector);
         },
 
@@ -16,52 +16,58 @@
                 if (match) {
                     if (m = match[1]) { // id
                         elem = document.getElementById(m);
-                        if (elem) elem.length = 1;
-                        return elem ? elem : null;
+                        if (elem) {
+                            this.length = 1;
+                            this[0] = elem;
+                        }
+                        return this;
                     } else if (m = match[2]) { // tag
                         elem = document.getElementsByTagName(m);
-                        return elem ? elem : null;
+                        return elem ? elem : this;
                     } else if (m = match[3]) { // class
                         elem = document.getElementsByClassName(m);
-                        return elem ? elem : null;
+                        return elem ? elem : this;
                     }
                 } else {
-                    return null;
+                    return this;
                 } 
             } else {
-                return null;
+                return this;
             }
         };
 
-    vlib.fn = {
+    minlib.fn = {
         length: 0,
         splice: splice
-    }
+    };
 
-    init.prototype = vlib.fn;
+    init.prototype = minlib.fn;
 
     // load function
-    vlib.fn.load = vlib.load = function(modulename) {
-        try {
-            if (vlib.fn[modulename] !== undefined) // loaded
-                return;
+    minlib.fn.load = minlib.load = function() {
+        var i, m = arguments,
+            len = arguments.length;
 
-            vlib.fn[modulename] = vlib[modulename] = module[modulename];
-        } catch(e) {
+        for (i = 0; i < len; i++) {
+            try {
+                if (minlib.fn[m[i]] !== undefined) // loaded
+                    return;
 
+                minlib.fn[m[i]] = minlib[m[i]] = module[m[i]];
+            } catch(e) {
+                // unexist
+            }
         }
     };
 
     // extend module
     var module = {
         extend: function() {
-            var i = 1,
-                k,
-                obj,
+            var i = 1, k, obj,
                 target = arguments[0] || {},
                 len = arguments.length;
 
-            if(len === 1) {
+            if (len === 1) {
                 return arguments[0];
             }
 
@@ -70,14 +76,57 @@
                 if (typeof obj === 'object') {
                     for (k in obj) {
                         target[k] = typeof target[k] === 'object' && typeof obj[k] === 'object' ?
-                            kss.extend(target[k], obj[k]) :
+                            this.extend(target[k], obj[k]) :
                             obj[k];
                     }
                 }
             }
             return target;
+        },
+        on: function(etype, callback) {
+            var i, len,
+                targets = this;
+
+            for (i = 0, len = targets.length; i < len; i++) {
+                if (targets[i].addEventListener) {
+                    targets[i].addEventListener(etype, function(event) {
+                        callback(event);
+                    });
+                } else {
+                    targets[i].attachEvent('on' + etype, function() {
+                        callback(window.event);
+                    });
+                }
+            }
+        },
+        ajax: function(settings) {
+            var xhr = ajaxHelper.xhr();
+
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    if (settings.dataType === 'json') {
+                        settings.success(JSON.parse(xhr.responseText));
+                    } else {
+                        settings.success(xhr.responseText);
+                    }
+                }
+            }
+            xhr.open(settings.type, settings.url, settings.async);
+            xhr.send();
         }
     };
 
-    window.vlib = window._ = vlib;
-}());
+    // ajax helper
+    var ajaxHelper = {
+        xhr: function() {
+            if (window.XMLHttpRequest) {
+                return new XMLHttpRequest();
+            } else {
+                return new ActiveXObject('Microsoft.XMLHTTP');
+            }
+        }
+    };
+
+    window.minlib = window._ = minlib;
+
+}(window));
